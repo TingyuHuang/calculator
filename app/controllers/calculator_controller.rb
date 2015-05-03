9bbe1f5fe7ea30fee41d5e7cee44cc41e5c45ba2
Tingyu Huang <soytingyuhuang@gmail.com>
2015-05-03 10:22:24 +0000

    Clean last_input in CalculatorController#index

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,25 +2,34 @@ class CalculatorController < ApplicationController
 	respond_to :json, :html
 
 	def index
-	end
-	def create
 		@client = Client.find_by(ip: request.remote_ip)
 
 		if @client.nil?
 			@client = Client.new
 			@client.ip = request.remote_ip
-			@client.last_input = String.new
 		end
+		@client.last_input = String.new
+
+
+		@client.save
+	end
+
+	def create
+		@client = Client.find_by(ip: request.remote_ip)
+		ret = {}
+
 		@client.last_input << params[:input]
 
-		if @client.last_input.end_with? "010-10"
+		if params[:input].eql? "="
 			@client.last_input = String.new
-			ret = "bingo!"
-		else
-			ret = @client.last_input
+		elsif @client.last_input.end_with? "010-10"
+			@client.last_input = String.new
+			ret[:html] = ""
 		end
+
 		@client.save
 
+		ret[:expression] = @client.last_input
 		render :json => ret
 	end
 end
